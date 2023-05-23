8b03e12935a98d414fca15b2405aa476fedd2224
Logan Patino <loganpatino10@gmail.com>
2023-05-23 19:42:34 -0400

    Initial commit with site skeleton

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,5 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+const tabs = defineCollection({})
 
-export const collections = { blog };
+export const collections = { tabs };
