0f3dd8595a4e9d6d1c777de9bb7aff32f7d31256
Logan Patino <loganpatino10@gmail.com>
2023-05-30 23:31:40 -0400

    Set main image per screen size

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [mdx(), sitemap()]
 });
