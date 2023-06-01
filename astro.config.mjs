2e2b573c730e01eb18bd7582041c46437ce8267e
Logan Patino <loganpatino10@gmail.com>
2023-06-01 01:42:12 -0400

    Add component for photo grid and flesh out some text

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
